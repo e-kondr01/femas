9cc1c412af5125fc30664d04891799140f49cd7b
e-kondr01 <e.kondr01@gmail.com>
2021-03-24 22:14:45 +0300

    url fix

--- femas_backend/products/urls.py
+++ femas_backend/products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<str:product_class>', ObjectListView.as_view()),
-    path('<str:product_class>/<int:pk>', ObjectDetailView.as_view()),
+    path('/<str:product_class>', ObjectListView.as_view()),
+    path('/<str:product_class>/<int:pk>', ObjectDetailView.as_view()),
     path('', AllProductsSearchView.as_view())
 ]
